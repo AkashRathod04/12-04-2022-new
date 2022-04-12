2c2ad76b5176b99ff414ebf387f8c6ffc2253b3b
om <omrathod77886@gmail.com>
2022-04-12 13:27:20 +0530

    new Changes

---
+++ Demo1.java
@@ -0,0 +1,24 @@
+public class Demo1{
+
+public void m1(){
+
+
+System.out.println("M1 is Method");
+
+
+}
+
+
+public static void main(String[]args){
+
+
+Demo1 de=new Demo1();
+de.m();
+
+
+
+
+}
+
+
+}
